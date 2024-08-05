5674c0a94d1ff2d70e429d9ff334dc4d4f8a7736
Bruce Merry <bmerry@sarao.ac.za>
2024-08-05 16:44:35 +0200

    Add pipelining to AsNumpy
    This speeds things up by allowing some overlap between CPU and GPU
    computation (although there is still no overlap between transfer and GPU
    computation).

--- src/katcbf_vlbi_resample/cupy_bridge.py
+++ src/katcbf_vlbi_resample/cupy_bridge.py
@@ -2,8 +2,12 @@
 
 """Convert streams to and from cupy."""
 
+from collections import deque
+from collections.abc import Iterator
 from typing import Self
 
+import cupy as cp
+import cupyx
 import xarray as xr
 
 from .stream import Stream
@@ -30,15 +34,33 @@ class AsCupy:
 class AsNumpy:
     """Transfer a stream from cupy to numpy."""
 
-    def __init__(self, input_data: Stream[xr.DataArray]) -> None:
+    def __init__(self, input_data: Stream[xr.DataArray], queue_depth: int = 1) -> None:
         self.time_base = input_data.time_base
         self.time_scale = input_data.time_scale
         self.channels = input_data.channels
         self.is_cupy = False
         self._input_it = iter(input_data)
+        self._queue: deque[tuple[xr.DataArray, cp.cuda.Event]] = deque()
+        self._queue_depth = queue_depth
 
-    def __iter__(self) -> Self:
-        return self
+    def _flush(self, max_size: int) -> Iterator[xr.DataArray]:
+        while len(self._queue) > max_size:
+            buffer, event = self._queue.popleft()
+            event.synchronize()
+            yield buffer
 
-    def __next__(self) -> xr.DataArray:
-        return next(self._input_it).as_numpy()
+    def __iter__(self) -> Iterator[xr.DataArray]:
+        for buffer in self._input_it:
+            out = cupyx.empty_like_pinned(buffer.data)
+            buffer = xr.DataArray(
+                cp.asnumpy(buffer.data, out=out, blocking=False),
+                dims=buffer.dims,
+                coords=buffer.coords,
+                name=buffer.name,
+                attrs=buffer.attrs,
+            )
+            event = cp.cuda.Event(disable_timing=True)
+            event.record()
+            self._queue.append((buffer, event))
+            yield from self._flush(self._queue_depth)
+        yield from self._flush(0)
