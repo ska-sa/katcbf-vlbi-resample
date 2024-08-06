Fix computation of the bandband
A passband specified as 0.95 only actually preserved 90% of the band
(discarding 5% on each end).

src/katcbf_vlbi_resample/resample.py
@@ -118,7 +118,7 @@ def _fir_coeff_win(taps: int, passband: float, ratio: Fraction, window: str = "h
     """Calculate coefficients for upfirdn filter."""
     fir = scipy.signal.firwin(
         taps,
-        [1 - passband, passband],
+        [0.5 - 0.5 * passband, 0.5 + 0.5 * passband],
         pass_zero="bandpass",
         window=window,
         fs=2 * ratio.denominator,