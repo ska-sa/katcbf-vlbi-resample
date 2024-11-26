9f0ec7c9bfcc56614088052f2bd8e9d4cf928197
Bruce Merry <bmerry@sarao.ac.za>
2024-11-26 15:59:32 +0200

    Add generic mechanism to NormalisePower to record values

--- src/katcbf_vlbi_resample/power.py
+++ src/katcbf_vlbi_resample/power.py
@@ -2,7 +2,11 @@
 
 """Normalise power level prior to quantisation."""
 
+from collections import deque
+from dataclasses import dataclass
+
 import cupy as cp
+import cupyx
 import numpy as np
 import xarray as xr
 
@@ -25,6 +29,14 @@ def _rms(array: np.ndarray | cp.ndarray) -> np.ndarray | cp.ndarray:
         return np.sqrt(np.square(array).mean(axis=-1))
 
 
+@dataclass(slots=True)
+class _RmsHistoryEntry:
+    start: int  # First sample index
+    length: int  # Number of samples
+    rms: xr.DataArray  # Numpy-backed RMS
+    event: cp.cuda.Event  # Event to wait for rms to be valid
+
+
 class NormalisePower(ChunkwiseStream[xr.DataArray, xr.DataArray]):
     """Normalise power level.
 
@@ -32,14 +44,68 @@ class NormalisePower(ChunkwiseStream[xr.DataArray, xr.DataArray]):
     chunk is `scale`. This is done independently for each time series. It
     may be beneficial to use :class:`.Rechunk` prior to this filter to
     control the chunk size.
+
+    Subclasses may override :meth:`record_rms` to store the RMS values to some
+    form of storage.
     """
+
+    _MAX_HISTORY = 64  # Maximum depth for rms_history deque
 
     def __init__(self, input_data: Stream[xr.DataArray], scale: float) -> None:
         super().__init__(input_data)
         self.scale = scale
+        self._rms_history: deque[_RmsHistoryEntry] = deque()
+
+    def record_rms(self, start: int, length: int, rms: xr.DataArray) -> None:
+        """Record the RMS values.
+
+        The base class does nothing. Subclasses may override it to take action.
+
+        The `rms` array is guaranteed to be backed by a numpy array rather than
+        GPU memory. To make this efficient when using cupy (and not block the
+        pipeline to transfer data to the host), there may be some delay between
+        the chunk being iterated and this callback.
+
+        Parameters
+        ----------
+        start
+            Sample index of the first sample in the chunk
+        length
+            Number of samples in the chunk
+        rms
+            RMS values from the chunk (with non-time dimensions preserved).
+        """
+        pass  # pragma: nocover
 
     def _transform(self, chunk: xr.DataArray) -> xr.DataArray:
         assert chunk.dtype.kind == "f", "only real floating-point data is supported"
         rms = xr.apply_ufunc(_rms, chunk, input_core_dims=[["time"]], output_dtypes=[chunk.dtype])
         chunk *= self.scale / rms  # TODO: is it safe to modify in place?
+
+        if isinstance(rms.data, cp.ndarray):
+            rms_out = cupyx.empty_like_pinned(rms.data)
+            rms_np = rms.copy(data=cp.asnumpy(rms.data, out=rms_out, blocking=False))
+            event = cp.cuda.Event(disable_timing=True)
+            event.record()
+            entry = _RmsHistoryEntry(chunk.attrs["time_bias"], chunk.sizes["time"], rms_np, event)
+            self._rms_history.append(entry)
+            if len(self._rms_history) > self._MAX_HISTORY:
+                self._rms_history[0].event.synchronize()
+            while self._rms_history and self._rms_history[0].event.done:
+                entry = self._rms_history.popleft()
+                self.record_rms(entry.start, entry.length, entry.rms)
+        else:
+            self.record_rms(chunk.attrs["time_bias"], chunk.sizes["time"], rms)
+
         return chunk
+
+    def __next__(self) -> xr.DataArray:
+        try:
+            return super().__next__()
+        except StopIteration:
+            # Flush out _rms_history
+            while self._rms_history:
+                entry = self._rms_history.popleft()
+                entry.event.synchronize()
+                self.record_rms(entry.start, entry.length, entry.rms)
+            raise
--- test/test_power.py
+++ test/test_power.py
@@ -4,13 +4,29 @@
 
 from fractions import Fraction
 
+import pytest
 import xarray as xr
 from astropy.time import Time
 
 from katcbf_vlbi_resample.power import NormalisePower
-from katcbf_vlbi_resample.utils import concat_time
+from katcbf_vlbi_resample.utils import concat_time, is_cupy
 
 from . import SimpleStream
+
+
+class StoreRms(NormalisePower):
+    """Subclass that stores RMS history in a list."""
+
+    _MAX_HISTORY = 5  # Override base class value to test flushing
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.history: list[tuple[int, int, xr.DataArray]] = []
+
+    def record_rms(self, start: int, length: int, rms: xr.DataArray) -> None:  # noqa: D102
+        assert not is_cupy(rms)
+        self.history.append((start, length, rms))
+        super().record_rms(start, length, rms)
 
 
 class TestNormalisePower:
@@ -20,13 +36,14 @@ class TestNormalisePower:
         """Test :class:`.NormalisePower`."""
         rng = xp.random.default_rng(seed=1)
         data = xr.DataArray(
-            rng.standard_normal(size=(2, 10000)) * 4.5,  # cupy doesn't implement Generator.normal
+            rng.standard_normal(size=(2, 100000)) * 4.5,  # cupy doesn't implement Generator.normal
             dims=("pol", "time"),
-            attrs={"time_bias": Fraction(123, 456)},
+            coords={"pol": ["h", "v"]},
+            attrs={"time_bias": 123},
         )
         data[0] *= 2  # Give the pols different scales
-        stream = SimpleStream(time_base, time_scale, data, 10)
-        norm = NormalisePower(stream, 1.5)
+        stream = SimpleStream(time_base, time_scale, data, 10000)
+        norm = StoreRms(stream, 1.5)
         assert norm.time_base == time_base
         assert norm.time_scale == time_scale
         assert norm.channels is None
@@ -38,3 +55,18 @@ class TestNormalisePower:
         # exact match.
         std = out.std(dim="time")
         xp.testing.assert_allclose(std.data, 1.5, rtol=0.02)
+        assert len(norm.history) == 10
+        for i, (start, length, rms) in enumerate(norm.history):
+            assert start == 123 + 10000 * i
+            assert length == 10000
+            assert rms[0] == pytest.approx(9.0, rel=0.02)
+            assert rms[1] == pytest.approx(4.5, rel=0.02)
+            # Check the metadata, by filling in identical data
+            expected = xr.DataArray(
+                rms.data,
+                dims=("pol",),
+                coords={"pol": ["h", "v"]},
+            )
+            print(rms)
+            print(expected)
+            xr.testing.assert_identical(rms, expected)
